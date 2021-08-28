592e2334a50b1062d486ec2736abc09963ff9ba4
aminul91 <sumon2907@gmail.com>
2021-08-28 23:48:35 +0200

    test

--- src/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from app.models import *
-# Register your models here.
-
-class linksSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = tutorials_paths
-        fields = '__all__'
--- src/app/urls.py
+++ src/app/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from app.views import *
+from app.views import home_view
 
 app_name = 'app'
 
 urlpatterns = [
     # url: '/', name = app:home
-    path('', HomeView.as_view(), name="home"),
-    path('tutor_topic/', TopicView.tutor_topic, name="tutor_topic"),
-    path('homepage/', HomeView.as_view(), name="homepage"),
-    # path('api_links/', views.AboutView.api_links, name="api_links"),
+    path('', home_view, name='home'),
 ]
