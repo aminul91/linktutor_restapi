73906b0785db99d3effe1c954a320a33f56c906c
aminul91 <sumon2907@gmail.com>
2021-08-29 21:04:44 +0200

    API datafetch at controller

--- src/app/urls.py
+++ src/app/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('tutor_topic/', TopicView.tutor_topic, name="tutor_topic"),
     path('homepage/', HomeView.as_view(), name="homepage"),
-    # path('api_links/', views.AboutView.api_links, name="api_links"),
 ]
