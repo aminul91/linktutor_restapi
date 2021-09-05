e82c32a897b4a1574bbf5c6677123bcc25b7b4ee
aminul91 <sumon2907@gmail.com>
2021-09-05 20:57:21 +0200

    unit test for REST API model added

--- src/tests/test_urls.py
+++ src/tests/test_urls.py
@@ -6,11 +6,12 @@ class TestUrls(SimpleTestCase):
     def test_get_url_resolve(self):
         url = reverse('tutorials')
         self.assertEqual(resolve(url).func.view_class, ApiView)
+        print("four")
     
     def test_insert_update_url_resolve(self):
         url = reverse('tutorial_insert')
-        print(resolve(url))
         self.assertEqual(resolve(url).func.view_class, ApiInsert)
+        print("five")
         
     
     
